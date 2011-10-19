56ea4c97d849f9c04fb043d3da4f886c1ca63036
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2011-10-20 01:09:17 +0530

    Corrected GTK window init

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ const Gtk = imports.gi.Gtk;
 const Gettext = imports.gettext;
 
 function start() {
-    Gtk.init (0, null);
 
     var w = new Gtk.Window ({title: "Gnome Transfers"});
     w.show ();
