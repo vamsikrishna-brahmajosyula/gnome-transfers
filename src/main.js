2c149dd049f318519ca44bcbe9a7d94998d09834
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2011-10-20 23:52:03 +0530

    more changes for basic screen support

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ const Gtk = imports.gi.Gtk;
 const Gdk = imports.gi.Gdk;
 
 const Gettext = imports.gettext;
+const STYLE_DIR_PATH = 
+
 
 function start() {
     
     let provider = new Gtk.CssProvider();
-    provider.load_from_path(Path.STYLE_DIR + "gtk-style.css");
     Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
                                              provider,
                                              600);
