dbc2999ec1df802fc80662f3d0738106200e0d07
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2011-10-20 23:35:19 +0530

    Added Git.init()

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ const Gtk = imports.gi.Gtk;
 
 const Gettext = imports.gettext;
 
-function start() {
-
+function start(args) {
+    Gtk.init(args)
     var w = new Gtk.Window ({title: "Gnome Transfers"});
     w.show ();
 
