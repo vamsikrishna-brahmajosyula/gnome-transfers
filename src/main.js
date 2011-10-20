77a55d317e6bcb1fdabcd8d28505000f431c6d50
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2011-10-20 23:46:52 +0530

    Added screen support to show the window

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 const Gtk = imports.gi.Gtk;
-
+const Gdk = imports.gi.Gdk;
 
 const Gettext = imports.gettext;
 
-function start(args) {
-    Gtk.init(args)
+function start() {
+    
+    let provider = new Gtk.CssProvider();
+    provider.load_from_path(Path.STYLE_DIR + "gtk-style.css");
+    Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
+                                             provider,
+                                             600);
+    
     var w = new Gtk.Window ({title: "Gnome Transfers"});
     w.show ();
 
