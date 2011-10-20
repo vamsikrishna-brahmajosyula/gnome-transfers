d71806b96dee38b9141e7b8eaabf16c1358890c4
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2011-10-21 01:07:05 +0530

    Removed duplicate declaration of Gtk

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 const Gtk = imports.gi.Gtk;
- 
-
-const Gtk = imports.gi.Gtk;
 const Application = imports.application;
 
-const Gettext = imports.gettext;
+
 
 function start() {
     let application = new Application.Application();
